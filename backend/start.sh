e7301a915c0defd08d0a7d785d454c821a88d63f
CheikhAhmadou <cheikhounacasset95@gmail.com>
2026-04-12 20:04:20 +0200

    Add Railway deployment config (start.sh, Procfile, railpack)

---
+++ backend/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+php artisan migrate --force
+php artisan db:seed --force
+php artisan storage:link
+php artisan config:cache
+php artisan route:cache
+
+php artisan serve --host=0.0.0.0 --port=${PORT:-8080}
